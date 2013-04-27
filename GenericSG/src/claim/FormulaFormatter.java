77ac7cb1c7eb71d14724b5e613ede3e918a115e9
Ahmed Abdelmeged <mohsen@ccs.neu.edu>
2013-04-26 22:48:38 -0400

    A flag to shorten names in formated formulas, prevented repeated formulas.

--- GenericSG/src/claim/FormulaFormatter.java
+++ GenericSG/src/claim/FormulaFormatter.java
@@ -1,5 +1,7 @@
 package claim;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.Stack;
 
 import example.minbasis.Basis;
@@ -11,17 +13,22 @@ import example.saddlepoint.Saddle;
  * 
  * */
 public class FormulaFormatter {
-	static String format(Formula f){
+	static String format(Formula f, boolean shortName){
 		StringBuilder sb = new StringBuilder();
 		Stack<Formula> formulas = new Stack<Formula>();
 		formulas.push(f);
+		Set<Class<?>> printed = new HashSet<Class<?>>();
 		boolean first = true;
-		while(!formulas.empty()){
-			f = formulas.pop();
+		outer: while(!formulas.empty()){
+			do{
+				if(formulas.empty()) break outer;
+				f = formulas.pop();
+			}while(printed.contains(f.getClass()));
+			printed.add(f.getClass());
 			if(!first) sb.append("\n");
 			if(first) first = false;
-			sb.append(f.getClass().getName());
-			formatArgumentList(sb, f.getParameters(), true);
+			formatClassName(sb, f.getClass(), shortName);
+			formatArgumentList(sb, f.getParameters(), true, shortName);
 			if(f instanceof Predicate){
 				sb.append(".");
 			}else{
@@ -35,13 +42,13 @@ public class FormulaFormatter {
 					}else{
 						throw new RuntimeException("New quantified type detected: "+ qf.getClass().getName());
 					}
-					formatVar(sb, qf.getVar(), true);
+					formatVar(sb, qf.getVar(), true, shortName);
 					sb.append(":");
-					formatSubFormula(sb, qf.getSubFormula());
+					formatSubFormula(sb, qf.getSubFormula(), shortName);
 					formulas.push(qf.getSubFormula());
 				}else if(f instanceof Compound){
 					Compound cf = (Compound) f;
-					formatSubFormula(sb, cf.getLeft());
+					formatSubFormula(sb, cf.getLeft(), shortName);
 					formulas.push(cf.getLeft());
 					if (f instanceof AndCompound) {
 						sb.append(" ∧ ");
@@ -50,21 +57,21 @@ public class FormulaFormatter {
 					}else{
 						throw new RuntimeException("New compound type detected: "+ cf.getClass().getName());
 					}
-					formatSubFormula(sb, cf.getRight());
+					formatSubFormula(sb, cf.getRight(), shortName);
 					formulas.push(cf.getRight());
 				}else if(f instanceof Negated){
 					Negated nf = (Negated) f;
 					sb.append("¬");
-					formatSubFormula(sb, nf.getSubFormula());
+					formatSubFormula(sb, nf.getSubFormula(), shortName);
 					formulas.push(nf.getSubFormula());
 				}else if(f instanceof Let){
 					Let<?> lf = (Let<?>) f;
 					sb.append("let ");
-					formatVar(sb, lf.getVar(), true);
+					formatVar(sb, lf.getVar(), true, shortName);
 					sb.append(" = ");
-					formatSubFormula(sb, lf.getFunction());
+					formatSubFormula(sb, lf.getFunction(), shortName);
 					sb.append(" in ");
-					formatSubFormula(sb, lf.getSubFormula());
+					formatSubFormula(sb, lf.getSubFormula(), shortName);
 					formulas.push(lf.getSubFormula());
 				}else{
 					throw new RuntimeException("New formula type detected: "+ f.getClass().getName());
@@ -74,35 +81,38 @@ public class FormulaFormatter {
 		return sb.toString();
 	}
 
-	static void formatArgumentList(StringBuilder sb, Var<?>[] params, boolean showTypes){
+	static void formatArgumentList(StringBuilder sb, Var<?>[] params, boolean showTypes, boolean shortName){
 		sb.append("(");
 		for(int i=0;i<params.length-1;i++){
-			formatVar(sb, params[i], showTypes);
+			formatVar(sb, params[i], showTypes, shortName);
 			sb.append(", ");
 		}
 		if(params.length > 0){
-			formatVar(sb, params[params.length-1], showTypes);
+			formatVar(sb, params[params.length-1], showTypes, shortName);
 		}
 		sb.append(")");
 	}	
 
-	static void formatVar(StringBuilder sb, Var<?> var, boolean showTypes){
+	static void formatVar(StringBuilder sb, Var<?> var, boolean showTypes, boolean shortName){
 		sb.append(var.name);
 		if(showTypes){
 			sb.append("∈");
-			sb.append(var.type.getName());
+			formatClassName(sb, var.type, shortName);
 		}
 	}
 
-	static void formatSubFormula(StringBuilder sb, Formula f){
-		sb.append(f.getClass().getName());
-		formatArgumentList(sb, f.getParameters(), false);
+	static void formatSubFormula(StringBuilder sb, Formula f, boolean shortName){
+		formatClassName(sb, f.getClass(), shortName);
+		formatArgumentList(sb, f.getParameters(), false, shortName);
 	}
 	
+	static void formatClassName(StringBuilder sb, Class<?> klass, boolean shortName){
+		sb.append(shortName?klass.getSimpleName():klass.getName());
+	}
 	public static void main(String[] args) {
-		System.out.println(format(new Saddle()));
+		System.out.println(format(new Saddle(), true));
 		System.out.println("====");
-		System.out.println(format(new Basis()));
-		
+		System.out.println(format(new Basis(), true));
+
 	}
 }
